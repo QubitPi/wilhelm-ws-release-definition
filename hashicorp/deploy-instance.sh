69ae670ec291234dc56adeebb64e01ede81a7db6
Jack <jack20220723@gmail.com>
2024-12-19 16:35:48 +0800

    Polish documentation

--- hashicorp/deploy-instance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-
-cd instances
-terraform init
-terraform validate
-terraform apply -auto-approve
-cd ../
--- hashicorp/publish-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-
-cd images
-packer init .
-packer validate .
-packer build .
-cd ../
